f90b0dd6facbbcce19642f5f7dbee323b3f8fe3f
Andreas Koehler <177153165+akdanz@users.noreply.github.com>
2024-08-01 11:00:19 +0200

    add basic instantiation of grpc objects, will not run due to missing Logging Extensions

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,8 +1,16 @@
 #include "pch.h"
 
 using namespace System;
+using namespace Grpc::Net::Client;
 
 int main(array<System::String ^> ^ args) {
   Console::WriteLine("Hello World!");
+
+  auto options = gcnew GrpcChannelOptions();
+  options->HttpHandler = gcnew System::Net::Http::WinHttpHandler();
+  auto channel = GrpcChannel::ForAddress("http://localhost:6001", options);
+
+  Console::WriteLine("Channel created successfully!");
+
   return 0;
 }
